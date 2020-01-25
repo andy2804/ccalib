5265413870f8e03c78298cefd0bc212b3a7f89d4
andy2804 <andy2804@github.com>
2020-01-25 19:05:57 +0100

    [WIP] Refactoring UI into Widgets

---
+++ src/imgui_widgets.cpp
@@ -0,0 +1,13 @@
+//
+// Created by andya on 23.01.20.
+//
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "imgui_widgets.h"
+
+#include <vector>
+
+namespace ccalib {
+
+} // namespace ccalib
---
+++ src/imgui_widgets.h
@@ -0,0 +1,13 @@
+//
+// Created by andya on 23.01.20.
+//
+
+#ifndef IMGUI_WIDGETS_H
+#define IMGUI_WIDGETS_H
+
+namespace ccalib {
+
+
+} // namespace ccalib
+
+#endif // FUNCTIONS_H
